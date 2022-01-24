b2d00cf6c7df977b36c1c4109b5733ec152deb9d
Francesca Pompi <francesca.pompi93@gmail.com>
2022-01-24 14:23:40 +0100

    creazione progetto

---
+++ src/main/java/org/generation/italy/ELearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformApplication.class, args);
+	}
+
+}
