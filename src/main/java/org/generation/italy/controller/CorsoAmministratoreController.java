0bcf740fe6057b7b4fc67ffd9e602d675b7d283d
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2022-01-25 18:04:22 +0100

    aagiunta form corso

---
+++ src/main/java/org/generation/italy/controller/CorsoAmministratoreController.java
@@ -0,0 +1,53 @@
+package org.generation.italy.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.generation.italy.model.Corso;
+import org.generation.italy.service.CorsoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/amministrazione/corsi")
+public class CorsoAmministratoreController {
+	
+	@Autowired
+	private CorsoService corsiService;
+	
+	@GetMapping
+	public String corsi(Model model) {		
+		return "/amministrazione/corsi/edit";
+	}
+	
+	
+	@GetMapping("/create")
+	public String creaCorso(Model model) {
+		model.addAttribute("corso", new Corso());
+		return "/amministrazione/corsi/edit";
+	}
+	
+	@PostMapping("/create")
+	public String doCreate(@Valid @ModelAttribute("corso") Corso formCorsi, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			List<ObjectError> allErrors = bindingResult.getAllErrors();
+			for (ObjectError e : allErrors) {
+				System.out.println(e);
+			}
+			
+			return "/amministrazione/corsi/edit";
+		}
+		
+		corsiService.save(formCorsi);
+		return "redirect:/corsi";	
+	}
+
+} 
--- src/main/java/org/generation/italy/service/CorsoService.java
+++ src/main/java/org/generation/italy/service/CorsoService.java
@@ -1,8 +1,10 @@
 package org.generation.italy.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.generation.italy.model.Corso;
+import org.generation.italy.model.Insegnante;
 import org.generation.italy.repository.CorsoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -21,4 +23,18 @@ public class CorsoService {
 	public Corso getById(Integer id) {
 		return repo.getById(id);
 	}
+	
+	public Corso save(Corso corso) {
+		if (corso.getDataCreazione() == null)
+			corso.setDataCreazione(LocalDate.now());
+		
+		return repo.save(corso);
+
+	}
+	
+	public void deleteById(Integer id) {
+		repo.deleteById(id);
+	}
+
 }
+
