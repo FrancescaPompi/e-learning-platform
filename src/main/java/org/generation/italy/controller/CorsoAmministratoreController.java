614c883e00df24c020d29d0088a6830d467751f7
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2022-01-26 18:02:31 +0100

    ):

--- src/main/java/org/generation/italy/controller/CorsoAmministratoreController.java
+++ src/main/java/org/generation/italy/controller/CorsoAmministratoreController.java
@@ -13,8 +13,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -32,13 +34,16 @@ public class CorsoAmministratoreController {
 	
 	@GetMapping("/create")
 	public String creaCorso(Model model) {
+		model.addAttribute("edit", false);
 		model.addAttribute("corso", new Corso());
 		return "/amministrazione/corsi/edit";
 	}
 	
 	@PostMapping("/create")
 	public String doCreate(@Valid @ModelAttribute("corso") Corso formCorsi, BindingResult bindingResult, Model model) {
+	
 		if(bindingResult.hasErrors()) {
+			model.addAttribute("edit", false);
 			List<ObjectError> allErrors = bindingResult.getAllErrors();
 			for (ObjectError e : allErrors) {
 				System.out.println(e);
@@ -46,11 +51,53 @@ public class CorsoAmministratoreController {
 			
 			return "/amministrazione/corsi/edit";
 		}
-		
+	
 		corsiService.save(formCorsi);
-		return "redirect:/corsi";	
+		return "redirect:/amministrazione/corsi/list";	
 	}
 	
 	
+	
+	@GetMapping ("/list")
+	public String list(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+		List<Corso> result;
+		if (keyword != null) {
+			result = corsiService.findByKeywordSortedByTitolo(keyword);
+			model.addAttribute("keyword", keyword);
+		} else
+			result = corsiService.findAllSortedByRecent();
+		model.addAttribute("list", result);
+		return "/amministrazione/corsi/list";
+	}
+	
+	
+	@GetMapping("/delete/{id}")
+	public String doDelete(@PathVariable("id") Integer id) {		
+		corsiService.deleteById(id);
+		return "redirect:/amministrazione/corsi/list";
+	}
+	
+	
+	
+	
+	@GetMapping("/edit/{id}")
+	public String modificaCorso(@PathVariable("id") Integer id, Model model) {	
+		model.addAttribute("edit", true);
+		model.addAttribute("corso", corsiService.getById(id));
+		return "/amministrazione/corsi/edit";
+	}
+	
+	@PostMapping("/edit({id}")
+	public String modificaCorso(@Valid @ModelAttribute("corso") Corso formCorsi, BindingResult bindingResult, Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("edit", true);
+						
+			return "/amministrazione/corsi/edit";
+		}
+		
+		corsiService.update(formCorsi);
+		return "redirect:/amministrazione/corsi/list";	
+	}
 
 } 
