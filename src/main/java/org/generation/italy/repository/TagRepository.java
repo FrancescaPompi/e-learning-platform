042d49ff30f546286bbd6703aebcf1963f4eae57
Francesca Pompi <francesca.pompi93@gmail.com>
2022-01-28 11:44:42 +0100

    aggiunti tag a form e dettagli corso

---
+++ src/main/java/org/generation/italy/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package org.generation.italy.repository;
+
+import org.generation.italy.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+}
