bceaf3ab3f6445ee3de727f84853f7baaadc6eb1
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2022-01-28 09:19:56 +0100

    )):

--- src/main/java/org/generation/italy/controller/CapitoloAmministrazioneController.java
+++ src/main/java/org/generation/italy/controller/CapitoloAmministrazioneController.java
@@ -1,6 +1,5 @@
 package org.generation.italy.controller;
 
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -13,28 +12,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/amministrazione/capitoli")
 public class CapitoloAmministrazioneController {
-	
+
 	@Autowired
 	private CapitoloService capService;
-	
+
 	@Autowired
 	private CorsoService corsoService;
+
+
+
 	
-	
-	
-	@GetMapping("/{corsoId}/create")
+	@GetMapping("create/{corsoId}")
 	public String creaCapitolo(@PathVariable("corsoId") Integer id, Model model) {
+		
 		model.addAttribute("edit", false);
 		model.addAttribute("capitolo", new Capitolo());
 		model.addAttribute("corso", corsoService.getById(id));
@@ -45,10 +45,10 @@ public class CapitoloAmministrazioneController {
 
 	@PostMapping("/{corsoId}/create")
 	public String doCreate(@PathVariable("corsoId") Integer id, @Valid @ModelAttribute("capitolo") Capitolo formCapitolo, BindingResult bindingResult, Model model) {
-	
+
 		if(bindingResult.hasErrors()) {
 			
-			model.addAttribute("edit", false);
+ 		model.addAttribute("edit", false);
 			model.addAttribute("corso", corsoService.getById(id));		
 			
 			return "/amministrazione/capitoli/edit";
@@ -59,77 +59,62 @@ public class CapitoloAmministrazioneController {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@GetMapping("/detail/{id}")
-	public String detail(@PathVariable("id") Integer id , Model model) {
-		model.addAttribute("capitoli", capService.getById(id));
-		return "/amministrazione/capitoli/detail";
-	}
 
-	
-	
-	
-	
-	
-	
-	@GetMapping("/delete/{id}")
-	public String doDelete(@PathVariable("id") Integer id) {		
-		capService.deleteById(id);
-		return "redirect:/amministrazione/corsi/detail";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	@GetMapping("/edit/{id}")
-	public String modificaCorso(@PathVariable("id") Integer id, Model model) {	
-		model.addAttribute("edit", true);
-		model.addAttribute("capitolo", capService.getById(id));
-		return "/amministrazione/capitoli/edit";
-	}
-	
-	@PostMapping("/edit/{id}")
-	public String modificaCorso(@Valid @ModelAttribute("capitolo") Capitolo formCapitolo, BindingResult bindingResult, Model model) {
-		
-		if(bindingResult.hasErrors()) {
-			model.addAttribute("capitolo", true);
-						
-			return "/amministrazione/capitoli/edit";
-		}
-		
-		capService.save(formCapitolo);
-	
-		return "redirect:/amministrazione/corsi/detail";	
-	}
-	
+@GetMapping("/delete/{corsoId}/{id}")
 
-	
-	
-	
-	
+public String doDelete(@PathVariable("id") Integer id, @PathVariable("corsoId") Integer corsoId, Model model) {		
+	capService.deleteById(id);
+
+	model.addAttribute("corso", corsoService.getById(corsoId));
+
+
+	return "redirect:/amministrazione/corsi/detail/{corsoId}";
 }
+
+}
+
+//	
+
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//
+
+//	
+//	
+//	
+//	
+//	
+//	
+//	@GetMapping("/delete/{id}")
+//	public String doDelete(@PathVariable("id") Integer id) {		
+//		capService.deleteById(id);
+//		return "redirect:/amministrazione/corsi/detail";
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+
